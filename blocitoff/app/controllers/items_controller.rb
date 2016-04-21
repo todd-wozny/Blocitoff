d91ee608e1474736f562183e7d8f77a679374586
Todd Wozny <toddwozny@gmail.com>
2016-04-21 21:55:25 +0000

    Pretty Much done with UserStory-4, User Profile

---
+++ blocitoff/app/controllers/items_controller.rb
@@ -0,0 +1,12 @@
+class ItemsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @item = @user.items.create(item_params)
+    redirect_to user_path(@user)
+  end
+ 
+  private
+    def item_params
+      params.require(:item).permit(:name)
+    end
+end
--- blocitoff/config/routes.rb
+++ blocitoff/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :users do
+    resources :items
+  end
+  
   get 'welcome/index'
 
   get 'welcome/about'
