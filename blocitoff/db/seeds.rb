5029917bb1fd664d56e54f2a64d24775b38d7884
Todd Wozny <toddwozny@gmail.com>
2016-04-25 19:54:29 +0000

    Seeding Data, completed

--- blocitoff/db/seeds.rb
+++ blocitoff/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+3.times do 
+  user = User.create(email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+end
+
+User.all.each do |user|
+  10.times do 
+    Item.create(name: Faker::Lorem.sentence, user: user)
+  end
+end
